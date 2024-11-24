cde6cced3db7f9890914c1cc1797f51fbfa8ffad
TaboTabo <65530655+Tsubonee@users.noreply.github.com>
2024-11-24 16:40:00 +0900

    add kadai file

---
+++ kadai_005/score.php
@@ -0,0 +1,25 @@
+
+<p>
+        <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        $result_add = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10 ;
+        $result_mean = $result_add / 10; 
+
+        echo "result add : {$result_add}";
+        echo '<br>' ;
+        echo "result mean : {$result_mean}";
+        ?>
+    </p>
+</body>
+
+</html>
